9aad85032bd1071ea9435e47d5b826b04762dd41
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-02-28 23:11:50 +0700

    ONCE AGAIN.

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,43 +13,44 @@ import { Contact } from '../components/home/04-contact'
 import { useEffect } from 'react'
 
 export const getStaticProps = async () => {
-  const res1 = await fetch(`${server}/api/events`)
+  // const res1 = await fetch(`${server}/api/events`)
 
-  if (res1.status !== 200) {
-    console.log('res from GSPaths in [id] of events: ', res1)
-    throw new Error(
-      `There was an error! Status code is ${res1.status}`
-    )
-  }
-  const res2 = await fetch(`${server}/api/events`)
+  // if (res1.status !== 200) {
+  //   console.log('res from GSPaths in [id] of events: ', res1)
+  //   throw new Error(
+  //     `There was an error! Status code is ${res1.status}`
+  //   )
+  // }
+  // const res2 = await fetch(`${server}/api/events`)
 
-  if (res2.status !== 200) {
-    console.log('res from GSPaths in [id] of events: ', res2)
-    throw new Error(
-      `There was an error! Status code is ${res1.status}`
-    )
-  }
+  // if (res2.status !== 200) {
+  //   console.log('res from GSPaths in [id] of events: ', res2)
+  //   throw new Error(
+  //     `There was an error! Status code is ${res1.status}`
+  //   )
+  // }
 
-  const evtData = await res1.json()
-  const albData = await res2.json()
-  console.log('data from gsPaths in [id] of events: ', evtData)
+  // const evtData = await res1.json()
+  // const albData = await res2.json()
+  // console.log('data from gsPaths in [id] of events: ', evtData)
 
-  const { resources, next_cursor: nextCursor } =
-    await search({ expression: 'folder=index/_gallery' })
+  // const { resources, next_cursor: nextCursor } =
+  //   await search({ expression: 'folder=index/_gallery' })
 
-  const images = mapImageResources(resources)
+  // const images = mapImageResources(resources)
 
-  return {
-    props: {
-      events: evtData,
-      albums: albData,
-      gallery: images,
-      nextCursor: nextCursor || false,
-    },
-  }
+  // return {
+  //   props: {
+  //     events: evtData,
+  //     albums: albData,
+  //     gallery: images,
+  //     nextCursor: nextCursor || false,
+  //   },
+  // }
 }
 
-export default function Home({ gallery, nextCursor }) {
+// export default function Home({ gallery, nextCursor }) {
+export default function Home() {
   return (
     <>
       <Head>
@@ -63,7 +64,7 @@ export default function Home({ gallery, nextCursor }) {
       <main>
         <Hero />
         <Services />
-        <Gallery images={gallery} nextCursor={nextCursor} />
+        {/* <Gallery images={gallery} nextCursor={nextCursor} /> */}
         <Contact />
       </main>
     </>
